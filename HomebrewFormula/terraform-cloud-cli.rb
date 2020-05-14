be7cd8b7bf3dba75b9b0de7f9e0250aaff70caed
goreleaserbot <goreleaser@carlosbecker.com>
2020-05-14 16:31:49 +0000

    Brew formula update for terraform-cloud-cli version v0.1.3

--- HomebrewFormula/terraform-cloud-cli.rb
+++ HomebrewFormula/terraform-cloud-cli.rb
@@ -2,16 +2,16 @@
 class TerraformCloudCli < Formula
   desc "Interactive CLI for operating workspaces in Terraform Cloud"
   homepage "https://github.com/bendrucker/terraform-cloud-cli"
-  version "0.1.2"
+  version "0.1.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/bendrucker/terraform-cloud-cli/releases/download/v0.1.2/terraform-cloud-cli_0.1.2_darwin_amd64.tar.gz"
-    sha256 "5b99060acafb2a976fa4ab1080bdd38a99169530be374dd12eb95c6dff77fc9b"
+    url "https://github.com/bendrucker/terraform-cloud-cli/releases/download/v0.1.3/terraform-cloud-cli_0.1.3_darwin_amd64.tar.gz"
+    sha256 "acc70a25087138e28e4ae9878e15f1ccf7516107f93e2efcb568f96fa52a5dcd"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/bendrucker/terraform-cloud-cli/releases/download/v0.1.2/terraform-cloud-cli_0.1.2_linux_amd64.tar.gz"
-      sha256 "2472bc06158c3a5c420906839d0d1911fd224868deff9d0270273b9cfcc05476"
+      url "https://github.com/bendrucker/terraform-cloud-cli/releases/download/v0.1.3/terraform-cloud-cli_0.1.3_linux_amd64.tar.gz"
+      sha256 "a0d3d3731e6bd444ae19e9247414e64f9355972e370785da79a9adf57ef1edc5"
     end
   end
 
