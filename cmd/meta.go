9645cf3cb8c445560b40043ac8ee3fa460a7b1af
Ben Drucker <bvdrucker@gmail.com>
2020-05-22 15:41:26 -0700

    use CredentialsSource to provide support for credential helper plugins

--- cmd/meta.go
+++ cmd/meta.go
@@ -3,10 +3,15 @@ package cmd
 import (
 	"flag"
 	"fmt"
+	"log"
+	"path/filepath"
+	"runtime"
 	"strings"
 
 	"github.com/hashicorp/go-tfe"
+	svchost "github.com/hashicorp/terraform-svchost"
 	"github.com/hashicorp/terraform/command/cliconfig"
+	"github.com/hashicorp/terraform/plugin/discovery"
 	"github.com/mitchellh/cli"
 )
 
@@ -32,12 +37,17 @@ func (m *Meta) LoadConfig(host string) error {
 			return diags.Err()
 		}
 
-		diags = config.Validate()
-		if diags.HasErrors() {
-			return diags.Err()
+		source, err := config.CredentialsSource(discovery.FindPlugins("credentials", globalPluginDirs()))
+		if err != nil {
+			return err
 		}
 
-		tfeConfig.Token = getToken(config, host)
+		creds, err := source.ForHost(svchost.Hostname(host))
+		if err != nil {
+			return err
+		}
+
+		tfeConfig.Token = creds.Token()
 	}
 
 	if tfeConfig.Token == "" {
@@ -77,4 +87,20 @@ func flagUsage(flags *flag.FlagSet) string {
 	})
 
 	return out
+}
+
+// https://github.com/hashicorp/terraform/blob/da67d86d7f45b7985f875ad473e13571be7aec45/plugins.go#L18
+func globalPluginDirs() []string {
+	var ret []string
+	// Look in ~/.terraform.d/plugins/ , or its equivalent on non-UNIX
+	dir, err := cliconfig.ConfigDir()
+	if err != nil {
+		log.Printf("[ERROR] Error finding global config directory: %s", err)
+	} else {
+		machineDir := fmt.Sprintf("%s_%s", runtime.GOOS, runtime.GOARCH)
+		ret = append(ret, filepath.Join(dir, "plugins"))
+		ret = append(ret, filepath.Join(dir, "plugins", machineDir))
+	}
+
+	return ret
 }
